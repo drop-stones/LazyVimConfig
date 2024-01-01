522a544f01d934892988e56a89817c2c86a3f030
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-01 21:43:48 +0900

    feat(coding): better-escape.nvim

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -4,4 +4,15 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     event = "InsertEnter",
   },
+  {
+    "max397574/better-escape.nvim",
+    event = "VeryLazy",
+    opts = {
+      mapping = { "jk", "ｊｋ" },
+      timeout = 300,
+      keys = function()
+        return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<Esc>l" or "<Esc>"
+      end,
+    },
+  },
 }
