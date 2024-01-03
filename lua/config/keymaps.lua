6d759c60a052ec809ef248b2e49067a2628b3ea3
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-04 08:30:38 +0900

    feat(config): load private(machine-local) configs if exist

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local has_private_keymaps, private_keymaps = pcall(require, "config.private.keymaps")
