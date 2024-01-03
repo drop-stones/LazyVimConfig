fd21f1660b57e7f824d1bcd43c4222f33f20681e
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-04 08:30:38 +0900

    feat(config): load private(machine-local) configs if exist

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,5 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local has_private_autocmds, private_autocmds = pcall(require, "config.private.autocmds")
