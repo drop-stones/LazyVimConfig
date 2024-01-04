ae233220daeb460ea5cf486517b6bdd3f448465b
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-04 16:19:57 +0900

    feat(autocmds): disable insertion of comment leader when entering new lines

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,4 +2,12 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- formatoptions (we need to create autocmd because fo is overwritten by ftplugin)
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions:remove({ "r", "o" })
+  end,
+})
+
 local has_private_autocmds, private_autocmds = pcall(require, "config.private.autocmds")
