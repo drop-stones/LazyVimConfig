3d8e2229957018bc4b92fd718e0613748c2e5ff4
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-04 11:22:19 +0900

    feat(lang): powershell support

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -31,4 +31,29 @@ return {
 
   -- YAML
   { import = "lazyvim.plugins.extras.lang.yaml" },
+
+  -- PowerShell (no parsers are maintained)
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Ensure mason installs powershell editor services
+        powershell_es = {},
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      -- Ensure mason installs netcoredbg
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "netcoredbg" })
+        end,
+      },
+    },
+  },
 }
