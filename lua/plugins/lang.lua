a49204270394079f5c90d916f1ff9b33691422f4
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-04 11:59:20 +0900

    feat(lang): bash/zsh support

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -56,4 +56,38 @@ return {
       },
     },
   },
+
+  -- Bash/Zsh
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "bash" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Ensure mason installs bash language server
+        bashls = {
+          filetypes = { "sh", "bash", "zsh" },
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      -- Ensure mason installs bash-debug-adapter
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "bash-debug-adapter" })
+        end,
+      },
+    },
+  },
 }
