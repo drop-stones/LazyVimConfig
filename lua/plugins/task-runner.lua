b0584d5360c9c60a2cb144c7e3d68cc259d05132
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-05 16:34:30 +0900

    feat(task-runner): overseer.nvim

---
+++ lua/plugins/task-runner.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      defaults = {
+        ["<leader>:"] = { name = "+task" },
+      },
+    },
+  },
+  {
+    "stevearc/overseer.nvim",
+    cmd = { "OverseerOpen", "OverseerRunCmd", "OverseerRun" },
+    opts = {},
+    keys = {
+      { "<leader>:o", "<Cmd>OverseerOpen<CR>", desc = "Open the overseer window" },
+      { "<leader>:x", "<Cmd>OverseerClose<CR>", desc = "Close the overseer window" },
+      { "<leader>:t", "<Cmd>OverseerToggle<CR>", desc = "Toggle the overseer window" },
+      { "<leader>:s", "<Cmd>OverseerSaveBundle<CR>", desc = "Save the current tasks to disk" },
+      { "<leader>:l", "<Cmd>OverseerLoadBundle<CR>", desc = "Load a saved task from disk" },
+      { "<leader>:d", "<Cmd>OverseerDeleteBundle<CR>", desc = "Delete a saved task bundle" },
+      { "<leader>:s", "<Cmd>OverseerRunCmd<CR>", desc = "Run a raw shell command" },
+      { "<leader>::", "<Cmd>OverseerRun<CR>", desc = "Run a task from a template" },
+      { "<leader>:b", "<Cmd>OverseerBuild<CR>", desc = "Open the task builder" },
+      { "<leader>:q", "<Cmd>OverseerQuickAction<CR>", desc = "Run an action on the most recent task" },
+      { "<leader>:a", "<Cmd>OverseerTaskAction<CR>", desc = "Select a task to run an action on" },
+      { "<leader>:i", "<Cmd>OverseerInfo<CR>", desc = "Display diagnostic information about overseer" },
+      { "<leader>:c", "<Cmd>OverseerClearCache<CR>", desc = "Clear the task cache" },
+    },
+  },
+}
