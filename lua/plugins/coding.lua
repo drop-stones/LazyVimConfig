e8ca59e17b5fc34350cb96a70e0b678bac431ad9
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-02-17 12:29:15 +0900

    feat(coding): zen-mode.nvim

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -50,4 +50,25 @@ return {
 
   -- Improved Yank and Put
   { import = "lazyvim.plugins.extras.coding.yanky" },
+
+  -- Zen mode
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = {
+      window = {
+        width = 1,
+      },
+      plugins = {
+        -- this will change the font size on wezterm when in zen mode
+        -- See alse also the Plugins/Wezterm section in this projects README
+        wezterm = {
+          enabled = false,
+          -- can be either an absolute font size or the number of incremental steps
+          font = "+20", -- (10% increase per step)
+        },
+      },
+    },
+    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+  },
 }
