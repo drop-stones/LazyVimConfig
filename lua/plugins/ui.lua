ee4e158a841611888a838df524192796151dce24
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-02-20 22:24:19 +0900

    feat(ui): remove unnecessary codes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -74,15 +74,9 @@ local header = {
 }
 
 return {
-  { "nvimdev/dashboard-nvim", enabled = false },
-  { "echasnovski/mini.starter", enabled = false },
-  -- Dashboard. This runs when neovim starts, and is what displays
-  -- the "LAZYVIM" banner.
+  { import = "lazyvim.plugins.extras.ui.alpha" },
   {
     "goolord/alpha-nvim",
-    event = "VimEnter",
-    enabled = true,
-    init = false,
     opts = function()
       local dashboard = require("alpha.themes.dashboard")
 
@@ -116,38 +110,6 @@ return {
       dashboard.section.footer.opts.hl = "AlphaFooter"
       dashboard.opts.layout[1].val = 8
       return dashboard
-    end,
-    config = function(_, dashboard)
-      -- close Lazy and re-open when the dashboard is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          once = true,
-          pattern = "AlphaReady",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
-
-      require("alpha").setup(dashboard.opts)
-
-      vim.api.nvim_create_autocmd("User", {
-        once = true,
-        pattern = "LazyVimStarted",
-        callback = function()
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          dashboard.section.footer.val = "⚡ Neovim loaded "
-            .. stats.loaded
-            .. "/"
-            .. stats.count
-            .. " plugins in "
-            .. ms
-            .. "ms"
-          pcall(vim.cmd.AlphaRedraw)
-        end,
-      })
     end,
   },
 }
