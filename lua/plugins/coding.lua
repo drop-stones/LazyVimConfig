82602508320e34e6218ad0d91092321267803ee5
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-07-12 20:42:55 +0900

    feat(better-escape): escape from lazygit by "jk"

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -23,11 +23,13 @@ return {
     },
   },
 
-  -- Escape by "ｊｋ" (in double-byte character)
+  -- Escape by "jk" and "ｊｋ" (in double-byte character)
   {
     "max397574/better-escape.nvim",
     event = "VeryLazy",
     opts = {
+      timeout = 150,
+      default_mappings = false,
       mappings = {
         i = {
           j = {
@@ -47,7 +49,14 @@ return {
         },
         t = {
           j = {
-            k = "<C-\\><C-N>",
+            k = function()
+              local buf_name = vim.api.nvim_buf_get_name(0)
+              local is_lazygit = string.find(buf_name, "term://.*lazygit.*")
+              if is_lazygit ~= nil then
+                return "<Esc>"
+              end
+              return "<C-\\><C-N>"
+            end,
           },
           ["ｊ"] = {
             ["ｋ"] = "<C-\\><C-N>",
@@ -70,7 +79,6 @@ return {
           },
         },
       },
-      timeout = 150,
     },
   },
 
