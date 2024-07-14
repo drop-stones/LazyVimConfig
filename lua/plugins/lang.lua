3389c256d8f355c7cf1c55af45b4f6f3aacd59fa
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-07-14 15:51:52 +0900

    feat(tree-sitter): disable indentation feature

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -18,6 +18,18 @@ return {
     end,
   },
 
+  -- TreeSitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      indent = {
+        -- disable indentation because it is experimental feature
+        -- See https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#indentation
+        enable = false,
+      },
+    },
+  },
+
   -- rust
   { import = "lazyvim.plugins.extras.lang.rust" },
 
