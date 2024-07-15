25d0ce53db640e157f8f7292555a9ed38c437b92
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-07-15 13:11:44 +0900

    feat(aerial): remove a custom keymap

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -57,10 +57,4 @@ return {
 
   -- code outline window
   { import = "lazyvim.plugins.extras.editor.aerial" },
-  {
-    "stevearc/aerial.nvim",
-    keys = {
-      { "<leader>a", "<cmd>AerialToggle<cr>", desc = "Toggle the aerial window" },
-    },
-  },
 }
