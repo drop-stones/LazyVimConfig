b6fef2d8e6e6f243185da368100cfb8a2fcdbb0e
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-07-20 14:46:20 +0900

    feat(which-key): add new icon rules

--- lua/plugins/icon.lua
+++ lua/plugins/icon.lua
@@ -1,30 +1,58 @@
+local direction_color = "blue"
+local position_color = "azure"
+
 return {
   {
     "folke/which-key.nvim",
     opts = {
       icons = {
         rules = {
+          -- nvim default keys
+          { pattern = "uppercase", icon = "󰬶", color = "green" },
+          { pattern = "lowercase", icon = "󰬵", color = "green" },
+          { pattern = "keywordprg", icon = "", color = "cyan" },
+
           -- verb
+          { pattern = "help", icon = "󰋖", color = "blue" },
+          { pattern = "delete", icon = "󰆴", color = "red" },
           { pattern = "inspect", icon = " ", color = "red" },
           { pattern = "paste", icon = "󰆒", color = "cyan" },
           { pattern = "clear", icon = "󱘝", color = "red" },
           { pattern = "eval", icon = "󱖦", color = "cyan" },
+          { pattern = "fold", icon = "", color = "cyan" },
+          { pattern = "visual", icon = "󰒅", color = "cyan" },
+          { pattern = "goto", icon = "", color = "cyan" },
+          { pattern = "replace", icon = "󰬲", color = "cyan" },
+          { pattern = "change", icon = "", color = "cyan" },
 
           -- noun
           { pattern = "list", icon = "", color = "cyan" },
           { pattern = "workspace", icon = "", color = "cyan" },
           { pattern = "widget", icon = "󱍕", color = "cyan" },
           { pattern = "line", icon = "", color = "cyan" },
+          { pattern = "keymaps", icon = "", color = "cyan" },
 
-          -- arrow
-          { pattern = "up", icon = "", color = "cyan" },
-          { pattern = "down", icon = "", color = "cyan" },
+          -- direction
+          { pattern = "up", icon = " ", color = direction_color },
+          { pattern = "down", icon = " ", color = direction_color },
+          { pattern = "right", icon = " ", color = direction_color },
+          { pattern = "left", icon = " ", color = direction_color },
 
-          -- keywordprg
-          { pattern = "keywordprg", icon = "", color = "cyan" },
+          -- position
+          { pattern = "next", icon = "󰒭", color = position_color },
+          { pattern = "prev", icon = "󰒮", color = position_color },
+          { pattern = "around", icon = "󰅪", color = position_color },
+          { pattern = "inside", icon = "󰅲", color = position_color },
+          { pattern = "matching", icon = "󰅪", color = position_color },
+
+          -- comment
+          { pattern = "comment", icon = "", color = "cyan" },
+
+          -- surround
+          { pattern = "surround", icon = "󰉾", color = "cyan" },
 
           -- dap
-          { pattern = "breakpoint", icon = "", color = "red" },
+          { pattern = "breakpoint", icon = "", color = "red" },
           { pattern = "run", icon = "", color = "cyan" },
           { pattern = "continue", icon = "", color = "cyan" },
           { pattern = "step in", icon = "", color = "cyan" },
@@ -32,6 +60,9 @@ return {
           { pattern = "step over", icon = "", color = "cyan" },
           { pattern = "pause", icon = "", color = "cyan" },
           { pattern = "terminate", icon = "", color = "cyan" },
+
+          -- flash.nvim
+          { pattern = "flash", icon = "⚡", color = "yellow" },
 
           -- note taking
           { pattern = "note", icon = "", color = "purple" },
