8a64bac4c0878c618424ab053dd6049e035cbbe5
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-07-22 20:45:12 +0900

    feat(flash): activate by default keymaps

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -6,14 +6,4 @@ return {
       min_pattern_length = 2,
     },
   },
-  -- stylua: ignore
-  keys = {
-    -- new keymaps
-    { "m", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "M", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-
-    -- disable keymaps
-    { "s", mode = { "n", "x", "o" }, false },
-    { "S", mode = { "n", "x", "o" }, false },
-  },
 }
