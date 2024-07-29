7c645d8c7857b857e7b7990e9acd226d92da20e9
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-07-29 21:34:46 +0900

    feat(telescope): add a new keymap to grep the open buffers

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- Search
+      { "<leader>sb", "<Cmd>Telescope live_grep grep_open_files=true<CR>", desc = "Buffer" },
+    },
+  },
+}
