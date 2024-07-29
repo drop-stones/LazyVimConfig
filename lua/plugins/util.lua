3684c2e5f0f2c9a6e376eacac37d4cc85483fd9e
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-07-29 20:16:24 +0900

    feat(telescope): remove plugin (telescope-zoxide)

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -5,43 +5,6 @@ return {
   -- Dot Files
   { import = "lazyvim.plugins.extras.util.dot" },
 
-  -- fuzzy finder
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "jvgrootveld/telescope-zoxide" },
-    keys = {
-      -- zoxide
-      {
-        "<leader>fz",
-        "<Cmd>Telescope zoxide list<CR>",
-        desc = "Find directories",
-      },
-
-      -- diffview
-      {
-        "<leader>gd",
-        "<Cmd>DiffviewOpen<CR>",
-        desc = "Open diffview",
-      },
-      {
-        "<leader>gH",
-        "<Cmd>DiffviewFileHistory<CR>",
-        desc = "Open file history",
-      },
-    },
-    opts = function()
-      require("telescope").load_extension("zoxide")
-      return {
-        defaults = {
-          mappings = {
-            n = {
-              ["jk"] = require("telescope.actions").close,
-            },
-          },
-        },
-      }
-    end,
-  },
   -- terminal
   {
     "folke/which-key.nvim",
