db81ee942ebf6ec92f601c9c36a6a0c508488356
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-08-03 01:04:43 +0900

    feat(tokyonight): change FlashLabel highlight

--- lua/plugins/motion/flash.lua
+++ lua/plugins/motion/flash.lua
@@ -1,8 +1,19 @@
 return {
-  "folke/flash.nvim",
-  opts = {
-    label = {
-      min_pattern_length = 2,
+  {
+    "folke/flash.nvim",
+    opts = {
+      label = {
+        min_pattern_length = 2,
+      },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      -- Change flash highlights
+      on_highlights = function(highlights, colors)
+        highlights.FlashLabel = { bg = colors.bg_highlight, fg = colors.magenta2, bold = true }
+      end,
     },
   },
 }
