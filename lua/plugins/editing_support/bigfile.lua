04fe2e6797fadd74772c5398210c93104c7a6065
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-08-04 22:36:09 +0900

    feat(bigfile): disable nvim-cmp in bigfile

--- lua/plugins/editing_support/bigfile.lua
+++ lua/plugins/editing_support/bigfile.lua
@@ -3,6 +3,28 @@ return {
   {
     "LunarVim/bigfile.nvim",
     event = "BufReadPre",
-    opts = {},
+    opts = {
+      filesize = 1, -- 1 MB
+      features = {
+        "lsp",
+        "treesitter",
+        "syntax",
+        "filetype",
+        "vimopts",
+        "matchparen",
+        {
+          name = "nvim-cmp",
+          disable = function()
+            require("cmp").setup.buffer({ enabled = false })
+          end,
+        },
+        {
+          name = "yanky.nvim",
+          disable = function()
+            require("lazyvim.util").get_plugin("yanky.nvim").keys = {}
+          end,
+        },
+      },
+    },
   },
 }
