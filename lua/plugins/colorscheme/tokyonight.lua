a62591abc531577a64bb44f7f1c3776dc14960b8
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-08-05 22:53:47 +0900

    feat(colorscheme): lighten line number highlights

--- lua/plugins/colorscheme/tokyonight.lua
+++ lua/plugins/colorscheme/tokyonight.lua
@@ -8,6 +8,17 @@ return {
         sidebars = "transparent",
         floats = "transparent",
       },
+      on_highlights = function(highlights, colors)
+        local util = require("tokyonight.util")
+
+        -- Line number
+        highlights.LineNr = { fg = util.lighten(highlights.LineNr.fg, 0.8) }
+        highlights.LineNrAbove = { fg = util.lighten(highlights.LineNrAbove.fg, 0.8) }
+        highlights.LineNrBelow = { fg = util.lighten(highlights.LineNrBelow.fg, 0.8) }
+
+        -- flash.nvim
+        highlights.FlashLabel = { bg = colors.bg_highlight, fg = colors.magenta2, bold = true }
+      end,
     },
   },
   {
--- lua/plugins/motion/flash.lua
+++ lua/plugins/motion/flash.lua
@@ -7,13 +7,4 @@ return {
       },
     },
   },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      -- Change flash highlights
-      on_highlights = function(highlights, colors)
-        highlights.FlashLabel = { bg = colors.bg_highlight, fg = colors.magenta2, bold = true }
-      end,
-    },
-  },
 }
