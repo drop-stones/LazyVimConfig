ebc84abdf7aaf55b9bdf2f97e2c9b6b58d50b587
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-09-07 10:18:05 +0900

    feat(chezmoi): Add filetypes to dotfiles

--- lua/plugins/dotfiles/chezmoi.lua
+++ lua/plugins/dotfiles/chezmoi.lua
@@ -1,3 +1,20 @@
+vim.filetype.add({
+  filename = {
+    ["dot_zshrc"] = "zsh",
+    ["dot_zshenv"] = "zsh",
+    ["dot_clang-format"] = "yaml",
+    ["dot_gitconfig"] = "gitconfig",
+  },
+  pattern = {
+    [".*/gitconfig/.*"] = "gitconfig",
+    -- TODO: Injections within template: https://github.com/nvim-treesitter/nvim-treesitter/discussions/1917
+    [".*%.sh%.tmpl"] = "sh",
+    [".*%.zsh%.tmpl"] = "zsh",
+    [".*%.ps1%.tmpl"] = "ps1",
+    [".*%.toml%.tmpl"] = "toml",
+  },
+})
+
 return {
   { import = "lazyvim.plugins.extras.util.chezmoi" },
 
