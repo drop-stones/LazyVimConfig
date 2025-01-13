66b3c92767bedcf3325d0ffaa28d61ab5d6768d0
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-01-13 21:03:56 +0900

    feat(keymap): Paste from clipboard by <C-v>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local map = vim.keymap.set
+
+-- Paste by '<C-v>'
+map({ "i", "c" }, "<C-v>", "<C-r>+")
+
 local has_private_keymaps, private_keymaps = pcall(require, "config.private.keymaps")
