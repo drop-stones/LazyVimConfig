92dc143951639df278ea25513a7247e375354df7
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-01-18 15:49:18 +0900

    feat(fzf): Disable "ctrl-v" to paste

--- lua/plugins/fuzzy_finder/fzf-lua/init.lua
+++ lua/plugins/fuzzy_finder/fzf-lua/init.lua
@@ -93,6 +93,21 @@ return {
         },
       }
 
+      local actions = require("fzf-lua.actions")
+      opts.actions = {
+        files = {
+          -- instead of the default action 'actions.file_edit_or_qf'
+          -- it's important to define all other actions here as this
+          -- table does not get merged with the global defaults
+          ["default"] = actions.file_edit,
+          ["ctrl-s"] = actions.file_split,
+          -- NOTE: Disable to paste from clipboard by fzf settings
+          -- ["ctrl-v"] = actions.file_vsplit,
+          ["ctrl-t"] = actions.file_tabedit,
+          ["alt-q"] = actions.file_sel_to_qf,
+        },
+      }
+
       return vim.tbl_deep_extend("force", fzf_opts, opts)
     end,
     keys = function(_, fzf_keys)
