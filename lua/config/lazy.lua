656fa852d519030fa8ccc9e61941df9f9e18892b
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-01-31 23:59:55 +0900

    chore(colorizer): remove nvim-colorizer.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,7 +11,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-    { import = "plugins.color" },
     { import = "plugins.colorscheme" },
     { import = "plugins.completion" },
     { import = "plugins.dap" },
