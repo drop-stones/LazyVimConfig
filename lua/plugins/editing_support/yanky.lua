241d44298a5e8d603fff541c5358accbe5cda05e
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-01-31 11:30:38 +0900

    feat(yanky.nvim): migrate history from sqlite3 to shada

--- lua/plugins/editing_support/yanky.lua
+++ lua/plugins/editing_support/yanky.lua
@@ -1,39 +1,4 @@
---- Check whether sqlite3.dll is installed or not
---- On windows, env.SQLITE3_DLL_PATH is required to be set to the path to sqlite3.dll
----@return boolean
-local is_sqlite3_dll_installed = function()
-  local luv = require("luv")
-  local os = luv.os_uname().sysname
-  if os == "Windows_NT" then
-    local path, _ = luv.os_getenv("SQLITE3_DLL_PATH")
-    if path ~= nil then
-      vim.g.sqlite_clib_path = path
-      return true
-    end
-  else
-    local path = vim.fn.exepath("sqlite3")
-    if path ~= nil then
-      return true
-    end
-  end
-  return false
-end
-
 return {
   -- Improved Yank and Put
   { import = "lazyvim.plugins.extras.coding.yanky" },
-  {
-    "gbprod/yanky.nvim",
-    dependencies = {
-      {
-        "kkharji/sqlite.lua",
-        enabled = is_sqlite3_dll_installed,
-      },
-    },
-    opts = {
-      ring = {
-        storage = (is_sqlite3_dll_installed() and "sqlite") or "shada",
-      },
-    },
-  },
 }
