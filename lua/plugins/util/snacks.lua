474d56114e9287e95c1cbf319344fbf9a1208be0
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-01 12:19:53 +0900

    chore(fzf-lua-git-search): remove fzf-lua-git-search

--- lua/plugins/util/snacks.lua
+++ lua/plugins/util/snacks.lua
@@ -10,40 +10,6 @@ return {
           },
         },
       },
-      dashboard = {
-        preset = {
-          -- Used by the `keys` section to show keymaps.
-          -- Set your custom keymaps here.
-          -- When using a function, the `items` argument are the default keymaps.
-          -- stylua: ignore
-          ---@type snacks.dashboard.Item[]
-          keys = {
-            { icon = " ", key = "f", desc = "Find File", action = require("fzf-lua-git-search").files },
-            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "g", desc = "Find Text", action = require("fzf-lua-git-search").live_grep },
-            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-            { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-          },
-          -- Used by the `header` section
-          header = [[
-███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-        },
-        sections = {
-          { pane = 1, header = "" }, -- padding section to centerize
-          { pane = 2, section = "header" },
-          { pane = 2, section = "keys", gap = 1, padding = 1 },
-          { pane = 2, section = "startup" },
-          { pane = 3, header = "" },
-        },
-      },
     },
   },
 }
