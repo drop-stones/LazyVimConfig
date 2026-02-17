2fe2b1c95bfbeb3155d66410b61fdd6a81c1185d
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-17 11:57:28 +0000

    chore(smart-splits-wsl2): remove smart-splits-wsl2

--- lua/plugins/navigation/smart-splits.lua
+++ lua/plugins/navigation/smart-splits.lua
@@ -1,6 +1,16 @@
 return {
   {
     "mrjones2014/smart-splits.nvim",
-    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "<c-h>", function() require("smart-splits").move_cursor_left()  end, mode = { "n", "t" }, desc = "Go to Left Window" },
+      { "<c-j>", function() require("smart-splits").move_cursor_down()  end, mode = { "n", "t" }, desc = "Go to Lower Window" },
+      { "<c-k>", function() require("smart-splits").move_cursor_up()    end, mode = { "n", "t" }, desc = "Go to Upper Window" },
+      { "<c-l>", function() require("smart-splits").move_cursor_right() end, mode = { "n", "t" }, desc = "Go to Right Window" },
+      { "<c-left>",  function() require("smart-splits").resize_left()  end, mode = { "n", "t" }, desc = "Resize Window Left" },
+      { "<c-right>", function() require("smart-splits").resize_right() end, mode = { "n", "t" }, desc = "Resize Window Right" },
+      { "<c-up>",    function() require("smart-splits").resize_up()    end, mode = { "n", "t" }, desc = "Resize Window Up" },
+      { "<c-down>",  function() require("smart-splits").resize_down()  end, mode = { "n", "t" }, desc = "Resize Window Down" },
+    },
   },
 }
